ee97a800f8295381b04374c4d5720fe4912a8cec
yetalex.hu <yetalex.hu@gmail.com>
2021-03-02 23:23:06 +0800

    http send代码

---
+++ Week 08/client.js
@@ -0,0 +1,60 @@
+class Request {
+  constructor(options) {
+    this.method = options.method || 'GET'
+    this.host = options.host
+    this.port = options.port || 80
+    this.path = options.path || '/'
+    this.headers = options.headers || {}
+    this.body = options.body || {}
+
+    if (!this.headers['Content-Type']) { // Content-Type 是http协议中必须有的
+      this.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    }
+
+    if (this.headers['Content-Type'] === 'application/json')
+      this.bodyText = JSON.stringify(this.body)
+    else if (this.headers['Content-Type'] === 'application/x-www-form-urlencoded')
+      this.bodyText = Object.keys(this.body).map(key => `${key}=${encodeURIComponent(this.body[key])}`).join('&')
+
+    this.headers['Content-Length'] = this.bodyText.length
+  }
+
+  send() {
+    return new Promise((resolve, reject) => {
+      const parser = new ResponseParser
+      resolve('')
+    })
+  }
+}
+
+class ResponseParser {
+  constructor() {}
+  receive(string) {
+    for (let i = 0; i < string.length; i++) {
+      this.receiveChar(string.charAt(i))
+    }
+  }
+  receiveChar(char) {
+    // 状态机代码 
+  }
+}
+
+void async function () {
+  let request = new Request({
+    method: 'POST',
+    host: '127.0.0.1',
+    port: '8088',
+    path: '/',
+    headers: {
+      ['X-Foo2']: 'customed'
+    },
+    body: {
+      name: 'winter'
+    }
+  })
+
+  let response = await request.send()
+
+  console.log(response)
+}()
+
