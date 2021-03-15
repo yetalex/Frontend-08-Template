d27bf4f2808969230f34c39a1d309c6359bc7123
yetalex.hu <yetalex.hu@gmail.com>
2021-03-16 01:20:51 +0800

    fix: 完成match.js代码

--- Week 11/NOTE.md
+++ Week 11/NOTE.md
@@ -1,1 +1,42 @@
 学习笔记
+
+从语法开始来建立知识框架。
+
+CSS
+  at-rules
+    @charset
+    @import
+    @media $$
+    @page
+    @counter-style
+    @keyframes $$
+    @fontface $$
+    @support
+    @namespace
+  rule
+    Selector
+      selector_group
+      selector
+        >
+        <sp>
+        +
+        ~
+      simple_selector
+        type
+        *
+        .
+        #
+        []
+        :
+        ::
+        :not()
+    Declaration
+      Key
+        variables
+        properties
+      Value
+        calc
+        number
+        length
+        ...
+
--- Week 11/match.js
+++ Week 11/match.js
@@ -3,6 +3,7 @@
  * 编写一个 match 函数。它接收两个参数，第一个参数是一个选择器字符串性质，第二个是一个 HTML 元素。这个元素你可以认为它一定会在一棵 DOM 树里面。通过选择器和 DOM 元素来判断，当前的元素是否能够匹配到我们的选择器。（不能使用任何内置的浏览器的函数，仅通过 DOM 的 parent 和 children 这些 API，来判断一个元素是否能够跟一个选择器相匹配。）
  * 
  * NOTE: 不考虑  伪类/伪元素, ||连接符， [attr=value] 不考虑其他情况
+ * 假设 > <sp> + ~  连接符只出现一次的情况
  * 
  * @param {*} selector 
  * @param {*} element 
@@ -11,17 +12,68 @@
 function match(selector, element) {
   if (!selector || !element.attributes) return false
 
-  // 1. > 分割，从右到左匹配
-  {
-    const rules = selector.split('>')
-  }
-  // 2. 空格分割，从右到左匹配
-  {
-    const rules = selector.split(' ')
-  }
+  let rules = selector.split('>')
   
-  // 3.在空格和> 分隔符匹配项里再匹配一下 「~ +」 兄弟分隔符
-  return true
+  // 1. > 分割，从右到左匹配
+  if (rules.length > 1) {
+    rules = rules.reverse()
+    let el = element
+    for(let rule of rules) {
+      if (!matchCombinedRule(rule, el)) return false
+      el = el.parentElement
+    }
+
+    return true
+  } else if ((rules = selector.split(' ')).length > 1) {
+    // 2. 空格分割，从右到左匹配
+    rules = rules.reverse()
+    let el = element
+    if (!matchCombinedRule(rules[0], el)) return false
+    for(let i=1; i < rules.length; i++) {
+      let rule = rules[i]
+      // el.parentElement,直到html,只要有一个元素匹配，则记录这个el，继续下一个规则
+      // 如果匹配到document.documentElement都没有，则 return false
+      let matched = false
+      while((el=el.parentElement) !== document.docuemntElement) {
+        if (matchCombinedRule(rule, el)) {
+          matched = true
+          break;
+        }
+      }
+      
+      if (!matched) return false
+    }
+
+    return true
+  } else if ((rules = selector.split('+')).length > 1) {
+    // 3.「+」 兄弟分隔符
+    rules = rules.reverse()
+    let el = element
+    for(let rule of rules) {
+      if (!el) return false     // el元素不存在
+      if (!matchCombinedRule(rule, el)) return false // 当前元素不匹配rule规则
+      el = el.previousSibling
+    }
+    return true
+  } else if ((rules = selector.split('~')).length > 1) {
+    rules = rules.reverse()
+    let el = element
+    if (!matchCombinedRule(rules[0], el)) return false
+    for (let i = 1; i < rules.length; i++) {
+      let rule = rules[i]
+      let matched = false
+      while (el = el.previousSibling) {
+        if (matchCombinedRule(rule, el)) {
+          matched = true
+          break;
+        }
+      }
+
+      if (!matched) return false
+    }
+
+    return true
+  }
 }
 
 /**
