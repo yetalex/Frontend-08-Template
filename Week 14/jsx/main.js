0df2606c61387a1c1efc28656c3bad4583c1f2f1
yetalex.hu <yetalex.hu@gmail.com>
2021-04-06 23:10:48 +0800

    fix: jsx基本用法

--- Week 14/jsx/main.js
+++ Week 14/jsx/main.js
@@ -1,5 +1,24 @@
-for (let i of [1, 2, 3]) {
-  console.log(i)
+function createElement(type, attributes, ...children) {
+  let element = document.createElement(type)
+  for(let name in attributes) {
+    element.setAttribute(name, attributes[name])
+  }
+  for(let child of children) {
+    if (typeof child === 'string') {
+      child = document.createTextNode(child)
+    }
+    element.appendChild(child)
+  }
+  return element
 }
 
-let a = <div />
+let a = <div id="a">
+  <span>a</span>
+  <span>b</span>
+  <span>c</span>
+</div>
+document.body.appendChild(a)
+
+// 文本节点
+let b = <div id="b">Hello World!</div>
+document.body.appendChild(b)
