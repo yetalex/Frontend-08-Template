71f9764d7e83e64a6744d3218e907232cbdbfbce
yetalex.hu <yetalex.hu@gmail.com>
2021-04-12 00:29:07 +0800

    feat: 初始化以下代码

---
+++ Week 15/carousel/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: 'development',
+  entry: './main.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [['@babel/plugin-transform-react-jsx', { pragma: 'createElement' }]]
+          }
+        }
+      }
+    ]
+  }
+}
