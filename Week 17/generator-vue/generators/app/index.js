5a72cac0be28b935c92ebcfad5fb1dabc5932d80
yetalex.hu <yetalex.hu@gmail.com>
2021-04-27 01:21:12 +0800

    fix: 完成一个简单的yeoman的demo

--- Week 17/generator-vue/generators/app/index.js
+++ Week 17/generator-vue/generators/app/index.js
@@ -16,7 +16,7 @@ module.exports = class extends Generator {
       "name": answer.name,
       "version": "1.0.0",
       "description": "",
-      "main": "index.js",
+      "main": "./src/index.js",
       "scripts": {
         "test": "echo \"Error: no test specified\" && exit 1"
       },
@@ -34,8 +34,31 @@ module.exports = class extends Generator {
     this.fs.extendJSON(this.destinationPath('package.json'), pkgJson)
     // 只要package.json发生变化，在yo vue的时候，就会自动执行 npm install
     await this.addDependencies(['vue'])
-    await this.addDevDependencies(['webpack', 'vue-loader'])
+    await this.addDevDependencies([
+      'webpack', 'webpack-cli', 'vue-loader', 'vue-style-loader', 'css-loader', 'vue-template-compiler', 'copy-webpack-plugin'])
     // this.npmInstall(['vue'], {'save-dev': false})  // v5.2.0 已经废弃
     // this.npmInstall(['webpack', 'vue-loader'], {'save-dev': true}) // v5.2.0已经废弃
+
+    this.fs.copyTpl(
+      this.templatePath('HelloWorld.vue'),
+      this.destinationPath('src/HelloWorld.vue'),
+      {}
+    )
+
+    this.fs.copyTpl(
+      this.templatePath('webpack.config.js'),
+      this.destinationPath('webpack.config.js')
+    )
+
+    this.fs.copyTpl(
+      this.templatePath('main.js'),
+      this.destinationPath('src/main.js')
+    )
+
+    this.fs.copyTpl(
+      this.templatePath('index.html'),
+      this.destinationPath('src/index.html'),
+      {title: answer.name}
+    )
   }
 }
---
+++ Week 17/vue-demo/webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/main.js',
+  module: {
+    rules: [
+      { test: /\.vue$/, use: 'vue-loader' },
+      { test: /\.css$/, use: [
+        'vue-style-loader',
+        'css-loader'
+      ] }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/*.html', to: '[name].[ext]' }
+      ]
+    })
+  ]
+}
